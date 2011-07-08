2d0f2cc4af4f54783603c163fce1e2eec20453fa
Bo-Yi Wu <appleboy.tw@gmail.com>
2011-07-08 22:37:27 +0800

    Update: add layout library

---
+++ application/views/template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo $site_name;?></title>
+<meta name="description" content="<?php echo $site_description; ?>" />
+<meta name="keywords" content="<?php echo $site_keywords; ?>" />
+<link rel="stylesheet" type="text/css" href="/includes/css/bravomix.css" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"></script>
+</head>
+    <body>
+        <?php echo $content . "\n";?>
+    </body>
+</html>
