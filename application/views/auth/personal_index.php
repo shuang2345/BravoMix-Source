2ff5709a128b7805475e7f70541a2ed131f2a64e
Bo-Yi Wu <appleboy.tw@gmail.com>
2011-07-14 20:06:14 +0800

    Fixed: short_tag error

--- application/views/auth/personal_index.php
+++ application/views/auth/personal_index.php
@@ -2,10 +2,10 @@
 
 <p>Hi,<?php echo $profile->user_name; ?>你好.歡迎來到BravoMix.</p>
 <p>你目前的身分是<?php echo $profile->group_description; ?>.</p>
-    <!--Users-->
-<?php if($profile->group_id == 1) { ?>
+
+<?php if($profile->group_id == 1): ?>
 <p><a href="<?php echo site_url('auth/index');?>">管理介面</a></p>
-<? }?>
+<?php endif; ?>
 <p><a href="<?php echo site_url('auth/personal_data');?>">個人資料</a></p>
 <p><a href="<?php echo site_url('auth/change_password');?>">忘記密碼</a></p>
 <p><a href="<?php echo site_url('auth/logout'); ?>">登出</a></p>
