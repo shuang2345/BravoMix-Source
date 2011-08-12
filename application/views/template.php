535ec28561ee8ffe951d3d76aba5ffb7ef0993af
Bo-Yi Wu <appleboy.tw@gmail.com>
2011-08-12 16:33:55 +0800

    Update: template layout

--- application/views/template.php
+++ application/views/template.php
@@ -5,7 +5,6 @@
 <title><?php echo $site_name;?></title>
 <meta name="description" content="<?php echo $site_description; ?>" />
 <meta name="keywords" content="<?php echo $site_keywords; ?>" />
-<link rel="stylesheet" type="text/css" href="/includes/css/bravomix.css" />
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"></script>
 </head>
