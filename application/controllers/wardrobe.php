3dd6e5531960583b0a111c3c689337d3c2283f8f
liaosankai <liaosankai@gmail.com>
2011-09-08 18:16:10 +0800

    因應資料庫調整做修復
    Signed-off-by: liaosankai <liaosankai@gmail.com>

--- application/controllers/wardrobe.php
+++ application/controllers/wardrobe.php
@@ -40,8 +40,10 @@ class Wardrobe extends MY_Controller {
 
         //從衣櫃中找出符合標籤(tag)的所有單品
         $items = $this->wardrobe_model->find_items($wardrobe_id, $tag_title);
+        
+        print_r($items);
 
-        //取得指定衣櫃可選用的所有標籤
+        //取得衣櫃可選用的所有標籤
         $tags = $this->wardrobe_model->find_tags($wardrobe_id);
 
         //組合視圖變數
@@ -59,7 +61,7 @@ class Wardrobe extends MY_Controller {
         }
         //繪出視圖
         $this->template->set_layout('template/layout/1-col');
-        $this->template->add_css('/assets/css/gallery/3-mini-paper-clip.css', 'screen');
+        $this->template->add_css('/assets/css/gallery/4-cork-borad.css', 'screen');
         $this->template->render('wardrobe/view', $data);
     }
 
@@ -67,7 +69,7 @@ class Wardrobe extends MY_Controller {
     /**
      * 衣櫃清單
      * 
-     * @todo 由於目前的需求，一位使用者只有一個衣櫃，故此action謹保留
+     * @todo 由於目前的需求，一位使用者只有一個衣櫃，故此action僅保留
      */
     public function roll()
     {
@@ -76,19 +78,20 @@ class Wardrobe extends MY_Controller {
 
     //--------------------------------------------------------------------------
     /**
-     * 追加單品至衣櫃中
+     * 為衣櫃中的單品貼上標籤
      * 
-     * 若沒有指定衣櫃代碼，使用登入者預設的衣櫃
+     * ※若沒有指定衣櫃代碼，使用登入者預設的衣櫃
+     * ※這個action通常用在ajax方式
      * 
-     * @param int $item_id 單品代碼
      * @param string $tag_title 標籤名稱(中文)※需要urldecode
-     * @param int $wardrobe_id 衣櫃代碼* 
+     * @param int $item_id 單品代碼     
+     * @param int $wardrobe_id 衣櫃代碼 
      */
-    public function attach_item($item_id=NULL, $tag_title=NULL, $wardrobe_id=NULL)
+    public function paste_tag($tag_title=NULL, $item_id=NULL, $wardrobe_id=NULL)
     {
         //若有POST或GET的資料，改用POST或GET的資料
         $item_id = ($this->input->get_post('item_id')) ?
-                $this->input->get_post('item_id') : intval(item_id);
+                $this->input->get_post('item_id') : $item_id;
         $tag_title = ($this->input->get_post('tag_title')) ?
                 urldecode($this->input->get_post('tag_title')) : urldecode($tag_title);
         $wardrobe_id = ($this->input->get_post('wardrobe_id')) ?
@@ -97,9 +100,87 @@ class Wardrobe extends MY_Controller {
         //若沒有指定衣櫃代碼，使用登入者的衣櫃代碼為預設衣櫃代碼
         $wardrobe_id = ($wardrobe_id) ? $wardrobe_id : $this->loginer->wardrobe_id;
 
-        //如果有指定單品代碼，再繼續
-        $respone['result'] = $this->wardrobe_model->add_item($item_id, $tag_title, $wardrobe_id);
-    
+        echo 'item_id:' . $item_id . "<br />\n";
+        //貼上標籤        
+        $result = $this->wardrobe_model->paste_tag($tag_title, $item_id, $wardrobe_id);
+
+        //如果以Ajax方式請求，以JSON格式輸出結果
+        if ($this->input->is_ajax_request())
+        {
+            $respone['result'] = $result;
+            echo json_encode($respone);
+            exit;
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    /**
+     * 撕掉衣櫃單品上的標籤
+     * 
+     * @param int $item_id 單品代碼
+     * @param string $tag_title 標籤名稱
+     * @param int $wardrobe_id 衣櫃代碼
+     * @return int 影響的筆數 
+     */
+    public function tear_tag($tag_title=NULL, $item_id=NULL, $wardrobe_id=NULL)
+    {
+        //若有POST或GET的資料，改用POST或GET的資料
+        $item_id = ($this->input->get_post('item_id')) ?
+                $this->input->get_post('item_id') : intval($item_id);
+        $tag_title = ($this->input->get_post('tag_title')) ?
+                urldecode($this->input->get_post('tag_title')) : urldecode($tag_title);
+        $wardrobe_id = ($this->input->get_post('wardrobe_id')) ?
+                $this->input->get_post('wardrobe_id') : intval($wardrobe_id);
+
+        //若沒有指定衣櫃代碼，使用登入者的衣櫃代碼為預設衣櫃代碼
+        $wardrobe_id = ($wardrobe_id) ? $wardrobe_id : $this->loginer->wardrobe_id;
+
+        //撕除標籤
+        $result = $this->wardrobe_model->tear_tag($tag_title, $wardrobe_id, $item_id);
+
+        //如果以Ajax方式請求，以JSON格式輸出結果
+        if ($this->input->is_ajax_request())
+        {
+            $respone['result'] = $result;
+            echo json_encode($respone);
+            exit;
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    /**
+     * 追加單品至衣櫃中
+     * 
+     * 若沒有指定衣櫃代碼，使用登入者預設的衣櫃
+     * 
+     * @param int $wardrobe_id 衣櫃代碼
+     * @param int $item_id 單品代碼
+     * @param string $tag_title 標籤名稱(中文)※需要urldecode     *
+     */
+    public function attach_item($wardrobe_id=NULL, $item_id=NULL, $tag_title=NULL)
+    {
+        //若有POST或GET的資料，改用POST或GET的資料
+        $wardrobe_id = ($this->input->get_post('wardrobe_id')) ?
+                $this->input->get_post('wardrobe_id') : intval($wardrobe_id);
+        $item_id = ($this->input->get_post('item_id')) ?
+                $this->input->get_post('item_id') : intval($item_id);
+        $tag_title = ($this->input->get_post('tag_title')) ?
+                urldecode($this->input->get_post('tag_title')) : urldecode($tag_title);
+
+        //若沒有指定衣櫃代碼，使用登入者的衣櫃代碼為預設衣櫃代碼
+        $wardrobe_id = ($wardrobe_id) ? $wardrobe_id : $this->loginer->wardrobe_id;
+
+        //追加單品
+        $result = $this->wardrobe_model->add_item($wardrobe_id, $item_id, $tag_title);
+
+        //如果以Ajax方式請求，以JSON格式輸出
+        if ($this->input->is_ajax_request())
+        {
+            $respone['result'] = $result;
+            echo json_encode($respone);
+            exit;
+        }
+        echo '追加單品結果:' . ($result) ? '成功' : '失敗';
     }
 
     //--------------------------------------------------------------------------
@@ -109,57 +190,30 @@ class Wardrobe extends MY_Controller {
      * 若沒有指定衣櫃代碼，使用登入者預設的衣櫃
      * 
      * @param int $item_id 單品代碼
-     * @param int $tag_title 標籤名稱(中文)※需要urldecode
      * @param int $wardrobe_id 衣櫃代碼 
      */
-    public function remove_item($item_id=NULL, $tag_title=NULL, $wardrobe_id=NULL)
+    public function remove_item($wardrobe_id=NULL, $item_id=NULL)
     {
         //若有POST或GET的資料，改用POST或GET的資料
-        $item_id = ($this->input->get_post('item_id')) ?
-                $this->input->get_post('item_id') : intval(item_id);
-        $tag_title = ($this->input->get_post('tag_title')) ?
-                urldecode($this->input->get_post('tag_title')) : urldecode($tag_title);
         $wardrobe_id = ($this->input->get_post('wardrobe_id')) ?
                 $this->input->get_post('wardrobe_id') : intval($wardrobe_id);
-        
+        $item_id = ($this->input->get_post('item_id')) ?
+                $this->input->get_post('item_id') : intval(item_id);
+
         //若沒有指定衣櫃代碼，使用登入者的衣櫃代碼為預設衣櫃代碼
         $wardrobe_id = ($wardrobe_id) ? $wardrobe_id : $this->loginer->wardrobe_id;
-        
-        //如果有指定單品代碼，再繼續
-        if ($item_id)
-        {
-            //檢查此單品是否不在衣櫃中
-            if (!$this->wardrobe_model->is_exists($item_id, $wardrobe_id, $tag_title))
-            {
-                $respone['error'] = '已經從衣櫃中移除';
-            }
-            else
-            {
-                //將單品從衣櫃中移除
-                if ($this->wardrobe_model->remove($item_id, $wardrobe_id, $tag_title))
-                {
-                    $respone['result'] = TRUE;
-                    $respone['error'] = NULL;
-                }
-                else
-                {
-                    $respone['error'] = '已經從衣櫃中移除';
-                }
-            }
-        }
-        else
-        {
-            $respone['error'] = '單品(' . $item_id . ')或衣櫃(' . $wardrobe_id . ')不存在';
-        }
+
+        //移除單品
+        $result = $this->wardrobe_model->remove_item($wardrobe_id, $item_id);
+
+        //如果以Ajax方式請求，以JSON格式輸出
         if ($this->input->is_ajax_request())
         {
+            $respone['result'] = $result;
             echo json_encode($respone);
             exit;
         }
-        else
-        {
-            redirect('wardrobe/view/' . $tag_title);
-        }
+        echo '追加單品結果:' . ($result) ? '成功' : '失敗';
     }
 
 }
